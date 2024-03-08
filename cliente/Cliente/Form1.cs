ac46a5b46f89b61fd64a06ccf57ef23323f181a0
agustina <poseagustina@gmail.com>
2024-03-08 12:29:43 -0300

    -

---
+++ cliente/Cliente/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Cliente
+{    
+
+    public partial class Form1 : Form
+    {
+
+        static private NetworkStream stream; 
+        static private StreamWriter streamw;
+        static private StreamReader streamr;
+        static private TcpClient cliente = new TcpClient(); //creo al cliente
+        static private string nombre;
+
+        private delegate void DAddItem(String s); //el delegate me sirve para pasar un método como parámetro
+        
+   
+              
+        
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //es el botón enviar
+        {
+            streamw.WriteLine(textBox1.Text); //mando al server el mensaje escrito
+            streamw.Flush();
+            textBox1.Clear();
+
+
+        }
+
+        private void agregoTextoListB(String s)
+        {
+            listBox1.Items.Add(s);
+        }
+
+
+        void Escuchar() //función del hilo
+        {
+            while (cliente.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DAddItem(agregoTextoListB), streamr.ReadLine()); //creo al delegado y el parámetro es la función que transfiere
+                                                                                     //los datos leídos del socket al listBox (para mostrar el mensaje en sí)
+                   
+                }
+                catch
+                {
+                    MessageBox.Show("No se ha podido conectar al servidor");
+                    Application.Exit();
+                }
+            }
+        }
+
+         void Conectar()
+        {
+            try
+            {
+                cliente.Connect("192.168.0.18", 6666); //me conecto al server
+                if (cliente.Connected)
+                {
+                    Thread t = new Thread(Escuchar); //creo al hilo para que se quede verificando el flujo de info (mensajes)
+
+                    stream = cliente.GetStream(); //obtengo el flujo de datos (socket)
+                    streamw = new StreamWriter(stream); //para leer
+                    streamr = new StreamReader(stream); //para escribir
+
+                    streamw.WriteLine(nombre); //mando el nombre de usuario x socket al server
+                    streamw.Flush(); //limpio
+
+                    t.Start(); //que se ejecute el hilo
+                }
+                else //si hubo problema al conectar
+                {
+                    MessageBox.Show("Servidor no Disponible");
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Servidor no Disponible");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //todo esto es para manejar la visibilidad de la interfaz al presionar el botón "Conectar"
+        {
+            label1.Visible = false;  
+            textBox2.Visible = false;
+            button2.Visible = false;
+            listBox1.Visible = true;
+            textBox1.Visible = true;
+            Enviar.Visible = true;
+
+            nombre = textBox2.Text;
+
+            Conectar(); //comienza la conexión
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ servidor/Servidor/Servidor_Chat.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading; //biblioteca para uso de hilos
+using System.Net.Sockets; //biblioteca para uso de sockets
+using System.IO;
+using System.Net;
+
+namespace Servidor
+{
+    class Servidor_Chat
+    {
+
+        private TcpListener server; //es el server, que esperará la conexión del Cliente
+        private TcpClient cliente = new TcpClient(); //el TcpClient me da la conexión entre el Server y el Cliente
+        private IPEndPoint datosServer = new IPEndPoint(IPAddress.Parse("192.168.0.16"), 6666); //ip y puerto del Server. 
+        private List<Connection> lista = new List<Connection>(); //lista que me guardará las conexiones
+
+        Connection con; //variable de tipo Connection (será la conexión propiamente dicha)
+
+
+        private struct Connection 
+        {
+            public NetworkStream stream; //me permite manejar el flujo de información (el envio de mensajes a través de los sockets)
+            public StreamWriter streamw; //escribir
+            public StreamReader streamr; //leer
+            public string nick; //lo que será el nombre/usuario del Cliente que se conecta
+        }
+
+        public Servidor_Chat()
+        {
+            Inicio();
+        }
+
+        public void Inicio()
+        {
+
+            Console.WriteLine("Servidor de Chat Agus encendido!");
+            server = new TcpListener(datosServer); //creo al server y lo pongo en escucha (esperando que se conecte algún cliente)
+            server.Start();
+
+            while (true) //ciclo while infinito para quedarme esperando clientes
+            {
+                cliente = server.AcceptTcpClient(); //acepto conexión de cliente
+
+                con = new Connection(); //creo la conexión
+                con.stream = cliente.GetStream(); //recibo el flujo de datos del cliente
+                con.streamr = new StreamReader(con.stream); //para leer el flujo de datos 
+                con.streamw = new StreamWriter(con.stream); //para escribir en ese flujo de datos
+
+                con.nick = con.streamr.ReadLine(); //nick (nombre) del cliente
+
+                lista.Add(con); //
+                Console.WriteLine(con.nick + " se ha conectado.");
+
+
+
+                Thread t = new Thread(Escuchar_conexion); //creo un hilo para verificar en paralelo el flujo de información (los mensajes que se envien)
+
+                t.Start();
+            }
+
+
+        }
+
+        void Escuchar_conexion()
+        {
+            Connection hcon = con;  //copio la conexión que ya tenía, así puedo reutilizar la variable "con" para una nueva posterior conexión
+
+            do //do while infinito
+            {
+                try
+                {
+                    string tmp = hcon.streamr.ReadLine(); //leo los caracteres que mandó el cliente
+                    Console.WriteLine(hcon.nick + ": " + tmp); //muestro el mensaje (nombre del usuario y lo que escribió)
+                    foreach (Connection c in lista) //recorro la lista de conexiones que tengo y si hay más mensajes los muestra. Luego limpia  
+                    {
+                        try
+                        {
+                            c.streamw.WriteLine(hcon.nick + ": " + tmp);
+                            c.streamw.Flush();
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+                catch //y si hubo algún problema al verificar los mensajes del cliente
+                {
+                    lista.Remove(hcon); //remueve la conexión
+                    Console.WriteLine(con.nick + " se ha desconectado.");
+                    break;
+                }
+            } while (true);
+        }
+
+    }
+}
